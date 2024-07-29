a191d73bbe4296ee04c3143f32ff90e8d6834549
S. Jaydev Acharya <167872959+JstJay@users.noreply.github.com>
2024-07-29 16:45:51 +0530

    Update Fdroid_Demo.java

--- 123/src/test/java/fdroid_Demo/Fdroid_Demo.java
+++ 123/src/test/java/fdroid_Demo/Fdroid_Demo.java
@@ -37,16 +37,11 @@ public class Fdroid_Demo extends Capabilities {
 		
 		@Test(enabled = false)
 		public void tcl() throws InterruptedException {
-			driver.findElement(MobileBy.AccessibilityId("Preference")).click();
-			driver.findElement(MobileBy.AccessibilityId("3. Preference dependencies")).click();
+			driver.findElement(MobileBy.AccessibilityId("name of the id")).click();
+			driver.findElement(MobileBy.AccessibilityId("id name")).click();
 			driver.findElement(MobileBy.id("android:id/checkbox")).click();
-			//driver.findElement(MobileBy.id("android:id/checkbox")).click();
-			
-			//Another way of using text is 
-			//If your using android to identify an element using an attribute then you can use ui selector
-			//method for selecting the attribute.
-			
-			driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"WiFi settings\")")).click();
+				
+			driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"text name\")")).click();
 			driver.findElement(MobileBy.id("android:id/edit")).sendKeys("sfdw");
 			driver.findElement(MobileBy.id("android:id/button2")).click();
 			
@@ -63,31 +58,25 @@ public class Fdroid_Demo extends Capabilities {
 			
 		}
  		
-/*		@Test(enabled = false) 
+		@Test(enabled = false) 
 		public void opennotificaitons() {
 			driver.openNotifications();
 			driver.findElement(MobileBy.className("android.widget.ImageView")).get(4).click();
-		}*/
+		}
 		
 		
 		@Test(enabled = false) 
 		public void scroll() {
 			driver.findElement(MobileBy.AccessibilityId("Views")).click();
-			/* If you want to scroll you need 4 things
-			 * 1. UIAutomater
-			 * 2. UiSelector
-			 * 3. UiScrollable
-			 * 4. scrollIntoView
-			 */
 			driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"WebView\"))");
 		}
 		
 		@Test(enabled = false) 
 		public void longpress() {
 			driver.findElement(MobileBy.AccessibilityId("Views")).click();
-			driver.findElement(MobileBy.AndroidUIAutomator("UISelector().text(\"Expandable Lists\")")).click();
-			driver.findElement(MobileBy.AndroidUIAutomator("UISelector().text(\"1. Custom Adapter\")")).click();
-			AndroidElement fishnames = driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"Fish Names\")"));
+			driver.findElement(MobileBy.AndroidUIAutomator("UISelector().text(\"text here\")")).click();
+			driver.findElement(MobileBy.AndroidUIAutomator("UISelector().text(\"text here\")")).click();
+			AndroidElement names = driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"names\")"));
 			
 			TouchAction ta = new TouchAction(driver);
 			ta.longPress(longPressOptions().withElement(element(fishnames)).withDuration(ofSeconds(3))).release().perform();
@@ -98,11 +87,11 @@ public class Fdroid_Demo extends Capabilities {
 		
 		  @Test(enabled = false) 
 		  	public void drag_and_drop() {
-			  driver.findElement(MobileBy.AccessibilityId("Views")).click();
-			  driver.findElement(MobileBy.AccessibilityId("Drag and Drop")).click();
+			  driver.findElement(MobileBy.AccessibilityId("text")).click();
+			  driver.findElement(MobileBy.AccessibilityId("Id Name")).click();
 			  
-			  AndroidElement ele1 = driver.findElement(MobileBy.id("io.appium.android.apis:id/drag_dot_1"));
-			  AndroidElement ele2 = driver.findElement(MobileBy.id("io.appium.android.apis:id/drag_dot_2"));
+			  AndroidElement ele1 = driver.findElement(MobileBy.id("org.fdroid.fdroid:id/drag_dot_1"));
+			  AndroidElement ele2 = driver.findElement(MobileBy.id("org.fdroid.fdroid:id/drag_dot_2"));
 			  
 			  TouchAction ta = new TouchAction(driver);
 			  ta.longPress(longPressOptions().withElement(element(ele1)).withDuration(ofSeconds(3))).moveTo(element(ele2)).release().perform();
@@ -111,9 +100,9 @@ public class Fdroid_Demo extends Capabilities {
 		  
 		  @Test(enabled = false) 
 		  public void typeswipe() {
-			driver.findElement(MobileBy.AccessibilityId("Views")).click();
-			driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"Date Widgets\")")).click();
-			driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"2. Inline\")")).click();
+			driver.findElement(MobileBy.AccessibilityId("text here")).click();
+			driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"Id name\")")).click();
+			driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"text")")).click();
 
 			AndroidElement ele1 = driver.findElement(MobileBy.AccessibilityId("12"));
 			AndroidElement ele2 = driver.findElement(MobileBy.AccessibilityId("7"));
@@ -131,9 +120,9 @@ public class Fdroid_Demo extends Capabilities {
 			driver.findElement(MobileBy.AccessibilityId("Enable SMS broadcasr receiver")).click();
 			driver.hideKeyboard();
 			
-			driver.findElement(MobileBy.id("io.appium.android.apis:id/sms_recipient")).sendKeys("(650)555-1212");
-			driver.findElement(MobileBy.id("io.appium.android.apis:id/sms_content")).sendKeys("hello there...");
-			driver.findElement(MobileBy.id("io.appium.android.apis:id/sms_send-message")).sendKeys("(650)555-1212");
+			driver.findElement(MobileBy.id("org.fdroid.fdroid/sms_recipient")).sendKeys("(650)555-1212");
+			driver.findElement(MobileBy.id("org.fdroid.fdroid/sms_content")).sendKeys("hello there...");
+			driver.findElement(MobileBy.id("org.fdroid.fdroid/sms_send-message")).sendKeys("(650)555-1212");
 			
 			//driver.pressKey(new KeyEvent(AndroidKey.APP_SWITCH));
 			
@@ -142,7 +131,7 @@ public class Fdroid_Demo extends Capabilities {
 			String exp = driver.findElement(MobileBy.AndroidUIAutomator("UiSelector().text(\"You: Hello There...\")")).getText();
 			System.out.println(exp);
 			
-			driver.activateApp("io.appium.android.apis");
+			driver.activateApp("org.fdroid.fdroid");
 			
 			
 		}
